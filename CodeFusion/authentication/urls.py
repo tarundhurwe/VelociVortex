f749c9e5a269cb9fa1d4327eebfbe45e2662bb73
tarundhurwe <dhurwetarun@gmail.com>
2023-11-18 22:42:47 +0530

    made changes for deployment

--- CodeFusion/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegistationView, LoginView
-
-urlpatterns = [
-        path("register", RegistationView.as_view(), name="registration"),
-        path("login", LoginView.as_view(), name="login")
-]
