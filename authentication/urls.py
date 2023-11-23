1cc25da22644ba4e92709a74342a7cc56245a329
tarundhurwe <dhurwetarun@gmail.com>
2023-11-23 22:41:42 +0530

    updated logout view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegistationView, LoginView, RefreshTokenView
+from .views import RegistationView, LoginView, RefreshTokenView, LogoutView
 
 urlpatterns = [
         path("register", RegistationView.as_view(), name="registration"),
         path("login", LoginView.as_view(), name="login"),
         path("refresh-token", RefreshTokenView.as_view(), name="refreshtoken"),
-        path("logout", LoginView.as_view(), name="logout")
+        path("logout", LogoutView.as_view(), name="logout")
 ]
